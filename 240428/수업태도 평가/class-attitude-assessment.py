6a283496faefca32d5d21ba573fc1ff317db2678
이안이다 <71820380+iank1m@users.noreply.github.com>
2024-04-28 23:38:38 +0900

    Title: 수업태도 평가, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240428/수업태도 평가/class-attitude-assessment.py
+++ 240428/수업태도 평가/class-attitude-assessment.py
@@ -1,21 +1,26 @@
 N = int(input())
+
+# 딕셔너리를 초기화합니다.
 mydict = {'Bessie': 0, 'Elsie': 0, 'Daisy': 0, 'Gertie': 0, 'Annabelle': 0, 'Maggie': 0, 'Henrietta': 0}
 
-# N개만큼 학생의 이름과 추가 점수가 공백을 기준으로 부여됩니다.
-for i in range(N):
-    name, score = input().split(' ')
+# 입력값을 받아 딕셔너리에 저장합니다.
+for _ in range(N):
+    name, score = input().split()
     mydict[name] += int(score)
 
+# 점수를 오름차순으로 정렬합니다.
+sorted_scores = sorted(set(mydict.values()))
+
 # 가장 낮은 점수를 찾습니다.
-lowest_score = min(mydict.values())
+lowest_score = sorted_scores[0]
 
 # 가장 낮은 점수를 받은 학생을 제외한 학생들 중 두 번째로 낮은 점수를 찾습니다.
-second_lowest_score = min(score for score in mydict.values() if score != lowest_score)
+second_lowest_score = min(score for score in sorted_scores if score != lowest_score)
 
 # 두 번째로 낮은 점수를 받은 학생의 이름을 찾습니다.
 second_lowest_students = [name for name, score in mydict.items() if score == second_lowest_score]
 
-# 부가 조건 다루기
+# 부가 조건을 처리합니다.
 if len(second_lowest_students) > 1 or len(set(mydict.values())) == 1:
     print("Tie")
 else:
