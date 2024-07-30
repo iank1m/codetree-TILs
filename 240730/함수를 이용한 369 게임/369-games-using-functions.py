9373a97ed2f0c0573388d673934a08c6c2ca399b
이안이다 <71820380+iank1m@users.noreply.github.com>
2024-07-30 10:14:42 +0900

    Title: 함수를 이용한 369 게임, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 240730/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240730/함수를 이용한 369 게임/369-games-using-functions.py
@@ -1,20 +1,17 @@
-a,b=map(int,input().split())
+a, b = map(int, input().split())
 
-arr = []
-for i in range(a,b+1):
-    arr.append(i)
-    
-def count_369(arr):
+def count_369(a, b):
     cnt = 0
-    k=1
-    i=0
-    while k<10*len(arr) and i<len(arr):
-        if arr[i]%k==3 or arr[i]%k==6 or arr[i]%k==9:
-            cnt +=1
-        elif arr[i]%3 == 0:
-            cnt += 1
-        i += 1
-        k = k*10
+    for i in range(a, b + 1):
+        k = 1
+        while k <= i:
+            if (i // k) % 10 in {3, 6, 9}:
+                cnt += 1
+                break  # 3, 6, 9를 포함하면 더 이상 확인할 필요 없음
+            k *= 10
+        else:
+            if i % 3 == 0:
+                cnt += 1
     return cnt
 
-print(count_369(arr))
+print(count_369(a, b))
